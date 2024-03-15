6808bc7f0b6bbd90dba69c82b9a211f6806ac166
Ana Victoria Pereira de Souza <ana.victoria@a.ucb.br>
2024-03-14 22:32:37 -0300

    primeira atividade

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,15 @@
+let num1 = 2;
+let num2 = 4;
+
+result = num1 + num2;
+console.log(`Resultado da SOMA entre ${num1} e ${num2} = ${result}`);
+
+result = num1 - num2;
+console.log(`Resultado da SUBTRAÇÂO entre ${num1} e ${num2} = ${result}`);
+
+result = num1 * num2;
+console.log(`Resultado da MULTIPLICAÇÃO entre ${num1} e ${num2} = ${result}`);
+
+result = num1 / num2;
+console.log(`Resultado da DIVISÃO entre ${num1} e ${num2} = ${result}`);
+
